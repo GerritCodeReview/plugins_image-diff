3b6aff771fd86c1573aa8f1708fe95704e572e5f
Raeggin Williams <raeggin@google.com>
2018-06-20 14:58:43 -0700

    Create opacity mode for image diff tool
    Change-Id: I15b55d2398b89b359a66fb680b6f22de0de53ea5

---
+++ gr-opacity-diff-mode/gr-opacity-diff-mode.js
@@ -0,0 +1,44 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  'use strict';
+  Polymer({
+    is: 'gr-opacity-diff-mode',
+
+    properties: {
+      baseImage: Object,
+      revisionImage: Object,
+      baseSrc: String,
+      revisionSrc: String,
+    },
+
+    attached() {
+      if (revisionImage !== undefined) {
+      const srcRevision = this.computeSrcString(revisionImage);
+      this.$$('#revisionImageEl').setAttribute('src', srcRevision);
+      }
+      if (baseImage !== undefined) {
+      const srcBase = this.computeSrcString(baseImage);
+      this.$$('#baseImageEl').setAttribute('src', srcBase);
+      },
+    },
+    handleOpacityChange() {
+      const opacity = this.$.opacitySlider.value;
+      this.$.revisionImageEl.style.opacity = opacity;
+    },
+    computeSrcString(image) {
+      return 'data:' + image['type'] + ';base64, ' + image['body'];
+    },
+  });
+})();
